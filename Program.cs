6d886bb1ff859bbe0519da0237763141d13517e6
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2017-08-14 09:48:46 +1200

    C# task2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10011845_task2_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
